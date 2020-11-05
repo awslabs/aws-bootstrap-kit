7f80c9b1d8a6a57c07b4401fce6d10091d5ecc79
chadvit <chadvit@amazon.com>
2020-11-05 11:31:05 +0100

    Add Prod account for integ testing

--- integTests/1_SDLC_Organization/bin/sdlc-organization.ts
+++ integTests/1_SDLC_Organization/bin/sdlc-organization.ts
@@ -34,6 +34,14 @@ stackProps = {
                 },
             ]
         },
+        {
+            name: 'Prod',
+            accounts: [
+                {
+                    name: 'WorkloadA-Prod'
+                }
+            ]
+        }
     ]
 };
 
