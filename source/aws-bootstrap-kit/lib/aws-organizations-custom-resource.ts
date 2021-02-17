e2c6cdde1a24819b79c47f256b489fb3e9ea7595
fbouteruche <francois.bouteruche.perso@live.fr>
2021-02-17 17:46:12 +0100

    Rework to keep the same logical ID for the orgCustomResource CloudFormation resource to avoid deletion of the existing orgCustomResource and thus the deletion of the existing AWS Organizations that will fail

--- source/aws-bootstrap-kit/lib/aws-organizations-custom-resource.ts
+++ source/aws-bootstrap-kit/lib/aws-organizations-custom-resource.ts
@@ -1,26 +1,66 @@
 import { Construct } from 'constructs';
+import * as path from 'path';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as iam from '@aws-cdk/aws-iam';
+import { Construct as CoreConstruct, CustomResource, Duration } from '@aws-cdk/core';
+import { Provider } from '@aws-cdk/custom-resources';
 
-import { Construct as CoreConstruct, CustomResource } from '@aws-cdk/core';
-import { AWSOrganizationProvider } from './organization-provider';
 
-
+/**
+* A Custom Resource construct capable of importing and creating AWS Organizations.
+* This construct uses the AWS CDK Custom Resources Provider Framework under the hood.
+*/
 export class AWSOrganizationsCustomResource extends CoreConstruct {
 
+  /**
+   * The Organization ID
+   */
   public readonly OrganizationID: string;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const organizationProvider = AWSOrganizationProvider.getOrCreate(this);
+    const onEventHandler = new lambda.SingletonFunction(this, 'OnEventHandler', {
+      code: lambda.Code.fromAsset(path.join(__dirname, 'organization-handler')),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.onEventHandler',
+      uuid: '0041F81C-A1F8-4F36-90AE-CD1564DC4392',
+      lambdaPurpose: 'OrganizationProvider',
+      timeout: Duration.minutes(1)
+    });
+
+    onEventHandler.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'organizations:CreateOrganization',
+          'organizations:DescribeOrganization',
+          'organizations:DeleteOrganization',
+          'organizations:ListAccounts'
+        ],
+        resources: ['*'],
+      }),
+    );
+
+    onEventHandler.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'iam:CreateServiceLinkedRole'
+        ],
+        resources: ['arn:aws:iam::*:role/aws-service-role/organizations.amazonaws.com/*'],
+        conditions: { StringLike: { 'iam:AWSServiceName': 'organizations.amazonaws.com' } }
+      }),
+    );
+
+    const provider = new Provider(this, 'OrganizationProvider', {
+      onEventHandler: onEventHandler
+    });
 
     let org = new CustomResource(
       this,
       `Resource`,
       {
-        serviceToken: organizationProvider.provider.serviceToken,
-        resourceType: "Custom::AWS",
-        properties: {
-        },
+        serviceToken: provider.serviceToken,
+        resourceType: "Custom::AWS"
       }
     );
 
