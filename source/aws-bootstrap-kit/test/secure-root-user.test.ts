c7ae42f59a8a895c94f50aad43ce8aed48bfc539
Jerome Van Der Linden <jeromevdl@gmail.com>
2021-12-10 13:58:50 +0100

    ssm serviceprincipal issue

--- source/aws-bootstrap-kit/test/secure-root-user.test.ts
+++ source/aws-bootstrap-kit/test/secure-root-user.test.ts
@@ -43,4 +43,24 @@ test("Get 2FA and Access key rules", () => {
   expectCDK(stack).to(
     haveResource("AWS::SNS::Topic")
   );
+
+  expectCDK(stack).to(
+    haveResource("AWS::IAM::Role", {
+      "AssumeRolePolicyDocument": {
+        "Statement": [
+          {
+            "Action": "sts:AssumeRole",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": [
+                "events.amazonaws.com",
+                "ssm.amazonaws.com"
+              ]
+            }
+          }
+        ],
+        "Version": "2012-10-17"
+      }
+    })
+  );
 });
