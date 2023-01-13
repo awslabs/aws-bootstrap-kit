912ed3a7f60562df6aa40a02928654f37e6d7f01
Florian Chazal <chazalf@amazon.com>
2023-01-13 17:12:09 +0100

    fix: remove duplicated jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  runner: "groups",
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  "collectCoverage":true
-};
